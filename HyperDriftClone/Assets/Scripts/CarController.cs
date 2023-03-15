0cdb492662d3f362af45b56dc34440090ede3d3f
Cem Güvenbaş <cemguvenbass@gmail.com>
2023-03-15 15:52:41 +0300

    Final Commit
    Final

--- HyperDriftClone/Assets/Scripts/CarController.cs
+++ HyperDriftClone/Assets/Scripts/CarController.cs
@@ -5,10 +5,22 @@ using UnityEngine;
 public class CarController : MonoBehaviour
 {
     [SerializeField] private float carSpeed;
+    [SerializeField] private float maxSpeed;
     Vector3 moveVec;
+
+    [SerializeField] float steerAngle;
+    float dragAmount= 0.99f;
+
+    [SerializeField] float traction;
 
     private void Update()
     {
         moveVec += transform.forward * carSpeed * Time.deltaTime;
+        transform.position += moveVec * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * steerAngle * Time.deltaTime * moveVec.magnitude);
+        moveVec *= dragAmount;
+        moveVec = Vector3.ClampMagnitude(moveVec, maxSpeed);
+        moveVec = Vector3.Lerp(moveVec.normalized,transform.forward,traction*Time.deltaTime)*moveVec.magnitude;
     }
 }
